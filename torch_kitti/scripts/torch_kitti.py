90e3880d64c74647ad10eeab52acb0d7dee9b898
Andrea Conti <contiandrea96@gmail.com>
2020-11-11 19:05:00 +0100

    Add download script

---
+++ torch_kitti/scripts/torch_kitti.py
@@ -0,0 +1,36 @@
+"""
+Main program for torch kitti
+"""
+
+from argparse import ArgumentParser
+
+from torch_kitti.raw.synced_rectified.download import download as sync_rect_download
+
+# dowload datasets
+
+download_funcs = {"sync_rectified": sync_rect_download}
+
+
+def main():
+
+    # parser
+    parser = ArgumentParser("Torch Kitti")
+    subparsers = parser.add_subparsers(help="sub-command help")
+
+    # download subparser
+    download_parser = subparsers.add_parser("download", help="scaffolding utilities")
+    download_parser.add_argument(
+        "name",
+        type=str,
+        choices=download_funcs.keys(),
+        help="name of the dataset to download",
+    )
+    download_parser.add_argument("path", type=str, help="where scaffold the dataset")
+    download_parser.set_defaults(func=lambda args: download_funcs[args.name](args.path))
+
+    # run
+    args = parser.parse_args()
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
